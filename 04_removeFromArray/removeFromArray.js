7bd28568d3f2307f633b1c3ef4f22e0caedd321a
gvteja99 <gvteja99@gmail.com>
2022-06-07 16:52:55 -0700

    Complete the 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let arr = args[0];
+    for(var i = 1; i < args.length; i++){
+        if (!arr.includes(args[i])) {
+            continue
+        }
 
+        var index = arr.indexOf(args[i]);
+        if (index > -1) {
+          arr.splice(index, 1);
+        }      
+    }
+    return arr
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
