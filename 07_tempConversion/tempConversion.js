f34ab958ae78dbee6c6cdb95adee503e74311648
gvteja99 <gvteja99@gmail.com>
2022-06-07 17:19:47 -0700

    Complete the 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(tempInFahrenheit) {
+  return +((tempInFahrenheit-32)*5/9).toFixed(1)
 };
 
-const ctof = function() {
-
+const ctof = function(tempInCelcius) {
+  return +((tempInCelcius * 9/5) + 32).toFixed(1)
 };
 
 // Do not edit below this line
