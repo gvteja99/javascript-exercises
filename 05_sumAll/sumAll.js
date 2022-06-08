799c0a7e608a6a04c19a4fe66186b9ae938a9ddc
gvteja99 <gvteja99@gmail.com>
2022-06-07 17:10:24 -0700

    Complete the 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (!(Number.isInteger(x) && Number.isInteger(y)) || (x<0||y<0)) {
+        return "ERROR"
+    }
 
+    if (x>y) {
+        y = [x, x = y][0];
+    }
+
+    x = x*(x-1)/2
+    y = y*(y+1)/2
+
+    return y-x    
 };
 
 // Do not edit below this line
