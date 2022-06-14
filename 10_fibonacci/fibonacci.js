cb7ece78a95140113622bc1292e88b67fec7b1a8
gvteja99 <gvteja99@gmail.com>
2022-06-13 21:08:16 -0700

    Finish 10_fibonacci in javascript-exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n<0) {
+        return "OOPS";
+    } else if (n<3) {
+        return 1;
+    }
+
+    if (typeof(n)==='string') {
+        n = +n;
+    }
+
+    let a = b = 1;
+    for (i=0; i<n-2; i++) {
+        let temp = a+b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
