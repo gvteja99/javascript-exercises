4887ee5ff7ea8b1c33b9aa03f57d1e71ede7ab97
gvteja99 <gvteja99@gmail.com>
2022-06-13 20:55:35 -0700

    Finish 08_calculator exercise in javascript-exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => total+item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total*item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	fact = 1
+  for (i=1; i<num+1; i++) {
+    fact*=i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
