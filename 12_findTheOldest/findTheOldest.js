c48f65af335d8d61808370391bd62aa2a375aabe
gvteja99 <gvteja99@gmail.com>
2022-06-13 22:04:02 -0700

    Finish 12_findTheOldest in javascript-exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    curr_year = new Date().getFullYear()
 
+    function compare(a, b) {
+        deathYearA = a['yearOfDeath']||curr_year;
+        deathYearB = b['yearOfDeath']||curr_year;
+        return (deathYearA - a['yearOfBirth'] <  deathYearB - b['yearOfBirth'])?1:-1
+    }
+    
+    return obj.sort((a, b) => compare(a, b))[0]
 };
 
 // Do not edit below this line
