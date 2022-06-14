8485e688e1aae1849058f3909fa4a5a85576ae07
gvteja99 <gvteja99@gmail.com>
2022-06-13 21:01:00 -0700

    Finish 09_palindromes in javascript-exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+    reverse_str = str.split("").reverse().join("");
+    return str === reverse_str
 };
 
 // Do not edit below this line
